efbffc23ec02c055a5e6da5b58f8e1ad927ebef3
ammar falmban <devAspirant@gmail.com>
2015-05-19 01:04:23 +0300

    التحويل من فهرنهايت الى الدرجة المؤوية
    برنامج بلغة السي هدفة التحويل  التحويل من فهرنهايت الى الدرجة المؤوية
    C Program that,s change from Fahrenheit to Celsius

--- FahrToCelusis/main.c
+++ FahrToCelusis/main.c
@@ -9,7 +9,24 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    // This Code to change the fahernhit to celusis tempreture
+    
+    float fahernhit, celsuis;
+    
+    float lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step  = 20;
+    
+    fahernhit = lower;
+    
+    while(fahernhit <= upper)
+    {
+        celsuis = (5.0 / 9.0) * (fahernhit - 32.0);
+        printf("%3.0f %6.1f \n ",fahernhit, celsuis);
+        fahernhit = fahernhit + step;
+    }
 }
